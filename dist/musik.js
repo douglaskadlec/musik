7f13ee190c0312df67a13bcc23fc489ef0fd7146
Douglas Kadlec <colorghost@outlook.com>
2026-01-04 15:48:59 -0800

    Add play and pause icons

--- dist/musik.js
+++ dist/musik.js
@@ -59,7 +59,7 @@
     function createPlayer(mount, config) {
         const shadow = mount.attachShadow({ mode: 'open' });
         const style = document.createElement('style');
-        style.textContent = `:host{font-family:inherit,system-ui,-apple-system,BlinkMacSystemFont,sans-serif;font-size:inherit;font-weight:inherit;color:inherit}*{padding:0;margin:0;box-sizing:border-box}.container{position:relative;display:grid;grid-template-columns:35% 1fr;gap:1.875em;width:100%}.container .progress{grid-column:span 2;height:1.25em;border-radius:.625em;overflow:hidden;cursor:pointer}.container .progress .bar{width:0;height:100%}.container .meta{cursor:default}.container .meta .image{display:block;width:100%;border-radius:.625em}.container .meta .title{text-overflow:ellipsis;overflow:hidden;white-space:nowrap}.container .playlist{display:flex;flex-direction:column}.container .playlist button{display:flex;align-items:center;padding:1em 1.125em;border-radius:.625em;border:none;background:none;font-family:inherit;font-size:inherit;font-weight:inherit;color:inherit;text-align:inherit;transition:.3s;cursor:pointer}.container .playlist button .track-duration{margin-left:auto}`;
+        style.textContent = `:host{font-family:inherit,system-ui,-apple-system,BlinkMacSystemFont,sans-serif;font-size:inherit;font-weight:inherit;color:inherit}*{padding:0;margin:0;box-sizing:border-box}.container{position:relative;display:grid;grid-template-columns:35% 1fr;gap:1.4em;width:100%}.container .progress{grid-column:span 2;height:1.2em;border-radius:.5em;overflow:hidden;cursor:pointer}.container .progress .bar{width:0;height:100%}.container .meta{cursor:default}.container .meta .image{display:block;width:100%;border-radius:.5em}.container .meta .title{text-overflow:ellipsis;overflow:hidden;white-space:nowrap}.container .playlist{display:flex;flex-direction:column}.container .playlist button{display:flex;align-items:center;column-gap:.4em;padding:.6em .7em;border-radius:.5em;border:none;background:none;font-family:inherit;font-size:inherit;font-weight:inherit;color:inherit;text-align:inherit;transition:.3s;cursor:pointer}.container .playlist button .play-icon,.container .playlist button .pause-icon{display:flex;width:1.3em}.container .playlist button .pause-icon{display:none}.container .playlist button .track-duration{margin-left:auto}`;
         const containerDiv = document.createElement('div');
         containerDiv.className = 'container';
         const progressSection = document.createElement('section');
@@ -85,12 +85,20 @@
                 button.className = 'active';
             }
             button.setAttribute('aria-label', `Play ${track.title} by ${track.artist}`);
+            const playIconDiv = document.createElement('div');
+            playIconDiv.className = 'play-icon';
+            playIconDiv.innerHTML = playIcon();
+            const pauseIconDiv = document.createElement('div');
+            pauseIconDiv.className = 'pause-icon';
+            pauseIconDiv.innerHTML = pauseIcon();
             const trackTitleP = document.createElement('p');
             trackTitleP.className = 'track-title';
             trackTitleP.textContent = track.title;
             const trackDurationSpan = document.createElement('span');
             trackDurationSpan.className = 'track-duration';
             trackDurationSpan.textContent = track.duration;
+            button.appendChild(playIconDiv);
+            button.appendChild(pauseIconDiv);
             button.appendChild(trackTitleP);
             button.appendChild(trackDurationSpan);
             playlistSection.appendChild(button);
@@ -104,5 +112,11 @@
         containerDiv.appendChild(playlistSection);
         shadow.appendChild(style);
         shadow.appendChild(containerDiv);
+    }
+    function playIcon() {
+        return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M187.2 100.9C174.8 94.1 159.8 94.4 147.6 101.6C135.4 108.8 128 121.9 128 136L128 504C128 518.1 135.5 531.2 147.6 538.4C159.7 545.6 174.8 545.9 187.2 539.1L523.2 355.1C536 348.1 544 334.6 544 320C544 305.4 536 291.9 523.2 284.9L187.2 100.9z"/></svg>`;
+    }
+    function pauseIcon() {
+        return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2026 Fonticons, Inc.--><path d="M176 96C149.5 96 128 117.5 128 144L128 496C128 522.5 149.5 544 176 544L240 544C266.5 544 288 522.5 288 496L288 144C288 117.5 266.5 96 240 96L176 96zM400 96C373.5 96 352 117.5 352 144L352 496C352 522.5 373.5 544 400 544L464 544C490.5 544 512 522.5 512 496L512 144C512 117.5 490.5 96 464 96L400 96z"/></svg>`;
     }
 })();
